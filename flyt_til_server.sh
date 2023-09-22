5d2a449460840c748fe484e12c96f07504535e64
asbjørn <qw41ha@student.aau.dk>
2023-09-22 23:02:35 +0200

    flytte shript

---
+++ flyt_til_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo cp ./index.html /var/www/html/
+sudo cp ./styles.css /var/www/html/
+sudo cp -r ./cv /var/www/html/
+sudo cp -r ./links /var/www/html/
+sudo cp -r ./cooking /var/www/html/
+sudo cp -r ./education /var/www/html/
+sudo cp -r ./projects /var/www/html/
